6350ca150b33bf4c2fa5801adb4630dc8638cda6
Vaibhav Sultane <vaibhavsultane1997@gmail.com>
2026-07-07 14:35:25 +0530

    day 04 completed here

---
+++ phase_01_htmlcssjs/day_04/index.js
@@ -0,0 +1,130 @@
+// // It is used to hold value
+
+// // name="aman"
+
+// // var, let , const
+
+// // var name=20
+// // var city="Rajpura"
+// // console.log(typeof city)
+
+// // var a=10
+// // var a=20  // redeclaration is possible in var
+
+// // a=40  // initialisation is possible here
+
+// // console.log(a)
+
+// // let , const
+
+
+ 
+// if(true){
+//     var a=20
+// }
+
+// console.log(a)
+
+// for(let i=3;i<30;i++){
+//  // i =29
+// }
+
+// console.log(i)
+
+
+// // let b=30
+
+
+// initialize
+
+// [a,fun]
+
+// [a,fun,b]  // uninitialized
+
+// console.log(a) // undefined
+// // console.log(b) // temperol dead zone
+
+// //  var is global or functional scoped
+// //  by using var you can redeclare same variable
+// //  variables can be accessed before initialization
+// var a=10
+// var a=30
+// // let is block scoped
+// //  by using let you can reinitialise variable only, redeclare not possible
+
+// let b=20
+// // let b=40
+// b=60
+
+// console.log(b)
+
+// // redeclaration and reinitialization both not possible here
+// const age=70
+
+// age=90
+
+// console.log(age)
+
+
+// for(let i=0;i<=10;i++){
+
+// }
+// let age=18
+// if(age>18){
+//     console.log("Person can vote")
+// }
+// else{
+//     console.log("can not vote")
+// }
+
+
+// ternary operator
+var runs=79
+
+runs > 100 ? console.log("Century") : console.log("Out")  
+
+
+let num=24 
+
+if(num==="24"){    
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+// == --> checks value
+// === --> check value and data type
+// !=  --> value
+// !== --> value + data type
+
+function nameInput(){
+
+}
+nameInput()
+
+var name=()=>{
+
+}
+name()
+
+
+let arr=[1,2,3,4]
+
+console.log(arr[2])
+
+let str="aman"
+
+let obj={
+    "name":"kartik",
+    "age":20
+}
+
+
+
+
+
+
+
+
+
+
