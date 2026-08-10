4b0acd05b4f3773ad48b1c081d54920c2fbf764c
Vaibhav Sultane <vaibhavsultane1997@gmail.com>
2026-08-10 20:22:42 +0530

    local storage+session storage updated

---
+++ phase_01_htmlcssjs/day_13/local_storage.js
@@ -0,0 +1,10 @@
+// Local Storage
+// It is an feature of web browser which allows you to 
+// store data permanently in your system
+
+// to store data in local storage
+localStorage.setItem("users","Aman") 
+// {"key":data} // {"users":"Aman"}
+
+// to retrive data
+localStorage.getItem("users")
